3c903bab78400317a7f0435923c33f6ae3190dac
burkfers <finrod09@users.noreply.github.com>
2023-12-15 18:28:32 +0100

    add untitled keyboard

---
+++ keyboards/untitledkeyboard/config.h
@@ -0,0 +1,10 @@
+// Copyright 2023 burkfers (@burkfers)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define MASTER_RIGHT
+
+#define USB_VBUS_PIN GP19
+
+#define OLED_DISPLAY_128X64
