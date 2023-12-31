9cd7c3701b5f890124efbdb9ce81385c43819c9b
Chris Trotter <christopher.trotter@tophatmonocle.com>
2023-12-30 20:00:49 -0500

    running down the rabbit hole of row2col-col2row differences and how matrix_scan works

--- keyboards/handwired/arcboard_mk19/matrix.c
+++ keyboards/handwired/arcboard_mk19/matrix.c
@@ -1,21 +1,17 @@
 // Copyright 2018-2022 Nick Brassel (@tzarc)
+// Copyright 2020-2023 alin m elena (@alinelena, @drFaustroll)
+// Copyright 2023 Stefan Kerkmann (@karlk90)
 // SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "quantum.h"
 #include "spi_master.h"
 
-// what are these used for? an array of row/col count.
-// why = ROWS? COLS is a default setting for col pins
-// rows was actually MATRIX_ROW_PINS, another default, in quantum/matrix.c
-// static SPLIT_MUTABLE_ROW pin_t row_pins[ROWS_PER_HAND] = MATRIX_ROW_PINS;
-// this seems to be just 'row/col count' so we can use an analogue
 static const uint16_t row_values[MATRIX_ROWS] = {0};
 static const uint16_t col_values[MATRIX_COLS] = {0};
 static matrix_row_t temp_matrix[MATRIX_ROWS] = {0};
 
-// the on-start block to get our SPI setup ready for matrix scanning
 void matrix_init_custom(void) {
     // we use two CS pins - one for each shift register chain, rows and cols
-    // setting CS to HIGH = 'not polling/selected'
     setPinOutput(SPI_MATRIX_CHIP_SELECT_PIN_COLS);
     writePinHigh(SPI_MATRIX_CHIP_SELECT_PIN_COLS);
 
@@ -25,13 +21,10 @@ void matrix_init_custom(void) {
     spi_init();
 }
 
-// we write row data to the 595 shift register chain
-// how does this work?
+// we write row data to the 74HC595 shift register chain
 static inline void write_to_row(uint16_t value) {
-    // message is length of 2; what is this doing exactly?
-    // this is the SPI data that is being written out to the shift registers
-    // KarlK90's code
-    uint8_t message[2] = {(value >> 8) & 0xFF, (uint8_t)(value & 0xFF)}; 
+    // message is length of 2; what is this doing exactly? KarlK90's code
+    uint8_t message[2] = {(value >> 8) & 0xFF, (uint8_t)(value & 0xFF)};
 
     spi_start(SPI_MATRIX_CHIP_SELECT_PIN_ROWS, false, 0, SPI_MATRIX_DIVISOR);
     spi_transmit(message, 2); // send data, length of 2 cuz 0xFF kind of format?
@@ -43,36 +36,44 @@ static inline void set_row_high(uint8_t row) {
     write_to_row(row_values[row]); 
 }
 
-// we need to read col data from the 589 shift register chain
-static inline uint8_t read_cols(void) {
-    // this should be the spi_receive block
-    // why is this doing stop/start...and the other is not? cuz copypasta, sir
+// we need to read col data from the 74HC589 shift register chain
+static inline uint8_t read_all_cols(void) {
+    // pretty sure we need to revisit this...needs a temp_cols_matrix or something?
+    uint8_t c = readPin(col_values[0]);
+
     spi_start(SPI_MATRIX_CHIP_SELECT_PIN_COLS, false, 0, SPI_MATRIX_DIVISOR); // cs pin, lsbFirst?, spi mode, spi divisor
     spi_receive((uint8_t*)temp_matrix, MATRIX_COLS_SHIFT_REGISTER_COUNT * sizeof(matrix_row_t));
     spi_stop(); 
-
-    uint8_t c = readPin(col_values[0]); // this is not needed
 
     for (uint8_t col = 1; col < MATRIX_COLS; col++) {
         c |= (readPin(col_values[col]) << col);
     }
     return c;
 }
+    // matrix.c handling rows
+    // looks like they shift every row high? then look at each col? 
+    // #elif (DIODE_DIRECTION == ROW2COL)
+    // Set col, read rows
+    // matrix_row_t row_shifter = MATRIX_ROW_SHIFTER;
+    // for (uint8_t current_col = 0; current_col < MATRIX_COLS; current_col++, row_shifter <<= 1) {
+    //     matrix_read_rows_on_col(curr_matrix, current_col, row_shifter);
+    // }
 
-// custom_matrix=lite - our custom matrix scan over SPI/shift registers
+
+// need to look at the original matrix.c...
 bool matrix_scan_custom(matrix_row_t current_matrix[]) {
-
     // we set each row HIGH, one by one, and poll all col state each time.
-    // we are looking for cols that are HIGH
+    // we need to build a fresh matrix scan so we can compare
     for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-        // set the row to HIGH
-        set_row_high(row);
+        set_row_high(row); // set this iteration's row shift register data output pin to HIGH
 
-        // now read the collective col state
-        uint8_t cols = read_cols();
-        // loop through the received cols state data
-        for (uint8_t col = 0; col < MATRIX_COLS; col++) {
-            current_matrix[col] |= (((cols & (1 << col)) ? 1 : 0) << row);
+        // now read the collective col state - this happens for each row
+        uint8_t temp_col_state = read_all_cols();
+        for (uint8_t current_col = 0; current_col < MATRIX_COLS; current_col++) {
+            // why is this current_matrix (mlego code)
+            // oh, they use prev/current(mlego) vs. current/temp(tzarc) vs. current/input(karl)
+            // qmk uses raw_matrix/curr_matrix
+            temp_matrix[col] |= (((temp_col_state & (1 << col)) ? 1 : 0) << row);
         }
     }
 
@@ -82,15 +83,5 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
         memcpy(current_matrix, temp_matrix, sizeof(temp_matrix));
     }
     return matrix_has_changed;
-
-
-/*
-    We want to:
-    - for each row
-      - set row HIGH
-      - get all cols HIGH/LOW
-    - compare this against our existing matrix
-    - return changed bool
-*/
 
 }
