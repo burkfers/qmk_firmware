e28c7cb3267b316c0294696b3439075f7227e61c
Chris Trotter <christopher.trotter@tophatmonocle.com>
2024-01-30 06:52:44 -0500

    dubious build script update for pin handedness

--- keyboards/handwired/arcboard_mk19/build.sh
+++ keyboards/handwired/arcboard_mk19/build.sh
@@ -52,4 +52,18 @@ elif [ "$1" == "right" ];then
     else
         echo "ERROR: Did not find an RP2 device in boot state!"
     fi
+elif [ "$1" == "flash" ];then
+        echo "double-press reset please!"
+    # sleep 5
+    echo "check for a bootable RP2040..."
+    RP2040_CHECK=`ioreg -p IOUSB | grep -o "RP2\ Boot" | grep -o "RP2"`
+    echo "RP2040_CHECK says: $RP2040_CHECK"
+    if [ "$RP2040_CHECK" == "RP2" ];then
+        echo "Found an RP2 device in boot state..."
+        echo "Copying the UF2 file onto the rpi boot volume..."
+        qmk flash -kb $KEYBOARD -km $KEYMAP -j $BUILD_CORES
+        echo "Now do an eeprom reset...unplug USB, hold down the far right key (e.g. F12)...and plug in USB."
+    else
+        echo "ERROR: Did not find an RP2 device in boot state!"
+    fi
 fi
