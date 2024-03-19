7fda15f9ad1f4adf709afed3817477d7bb3dcc27
burkfers <finrod09@users.noreply.github.com>
2024-03-19 17:13:09 +0100

    Add argos mouse as copy of dilemma

---
+++ keyboards/bastardkb/argos/3x5_3/keymaps/via/rules.mk
@@ -0,0 +1,13 @@
+ENCODER_MAP_ENABLE = yes
+CONSOLE_ENABLE = yes
+COMBO_ENABLE = yes
+
+MACCEL_ENABLE = yes
+VIA_ENABLE = yes
+ifeq ($(strip $(MACCEL_ENABLE)), yes)
+	SRC += ./maccel.c
+	ifeq ($(strip $(VIA_ENABLE)), yes)
+		SRC += ./maccel_via.c
+	endif
+	OPT_DEFS += -DMACCEL_ENABLE
+endif
