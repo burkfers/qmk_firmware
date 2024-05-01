54936a27687bc7d2c6b1e59cbddbd3abdab0cb5c
burkfers <finrod09@users.noreply.github.com>
2024-05-01 09:22:54 +0200

    dilemma/3x5_2: remove unneccessary disabling of audio support

--- keyboards/bastardkb/dilemma/3x5_2/rules.mk
+++ keyboards/bastardkb/dilemma/3x5_2/rules.mk
@@ -1,7 +1,6 @@
 # Build Options
 #   change yes to no to disable
 #
-AUDIO_SUPPORTED = no        # Audio is not supported
 RGB_MATRIX_SUPPORTED = no   # RGB matrix is supported and enabled by default
 RGBLIGHT_SUPPORTED = no     # RGB underglow is supported, but not enabled by default
 
